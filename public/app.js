38a97f5699f642f670f34d13db9f27779fb57a94
Mark-Kil <markkilcanarias@gmail.com>
2023-10-22 15:19:15 +0200

    radio section wotking

--- public/app.js
+++ public/app.js
@@ -7,20 +7,22 @@ formData.addEventListener("submit", (e) => {
     let email = document.getElementById("email");
     let age = document.getElementById("age");
     let description = document.getElementById("dropdown-menu");
+    let recommend = document.getElementsByName("recommend");
     let comment = document.getElementById("text_box");
 
-    if (name.value == "" || email.value == "" || age.value == ""|| description.value == "" || comment.value == "") {
+    if (name.value == "" || email.value == "" || age.value == ""|| description.value == "" || recommend.value == "" || comment.value == "") {
         alert("Ensure you fill out all the fields!");
       } else {
         alert("This form has been successfully submitted!");
         console.log(
-          `This form's data is:  Name: ${name.value}, Email: ${email.value}, Age: ${age.value}, Description: ${description.value}, Comment: ${comment.value}`
+          `This form's data is:  Name: ${name.value}, Email: ${email.value}, Age: ${age.value}, Description: ${description.value}, Recommend: ${document.querySelector('input[name="recommend"]:checked').value}, Comment: ${comment.value}`
         );
     
         name.value = "";
         email.value = "";
         age.value = "";
         description.value = "";
+        recommend.value = "";
         comment.value = "";
       }
     });
